328adbd11f429497c7f1b591d3f6f0a9630c7688
hye1ee <hyewon0809@kaist.ac.kr>
2024-05-01 16:13:13 -0400

    [feat] Implement panning

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, OrthographicCamera } from "@react-three/drei";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Canvas
+      shadows
+      raycaster={{ params: { Line: { threshold: 0.15 } } }}
+      camera={{ position: [-10, 10, 10], fov: 20 }}
+    >
+      <ambientLight intensity={0.5} />
+      <directionalLight
+        castShadow
+        position={[2.5, 10, 5]}
+        intensity={0.8}
+        shadow-mapSize={[1024, 1024]}
+      >
+        <orthographicCamera
+          attach="shadow-camera"
+          args={[-5, 5, 5, -5, 1, 50]}
+        />
+      </directionalLight>{" "}
+      <mesh position={[0.75, 0.5, 1]} castShadow receiveShadow>
+        <boxGeometry args={[0.5, 1, 0.5]} />
+        <meshStandardMaterial />
+      </mesh>
+      <mesh position={[-0.75, 0.5, -1]} castShadow receiveShadow>
+        <boxGeometry args={[0.5, 1, 0.5]} />
+        <meshStandardMaterial />
+      </mesh>
+      <mesh scale={20} receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+        <planeGeometry />
+        <shadowMaterial transparent opacity={0.1} />
+      </mesh>
+      <OrbitControls enableRotate={false} />
+      <pointLight intensity={0.75} position={[5, 5, 5]} />
+    </Canvas>
+  );
 }
 
-export default App
+export default App;
